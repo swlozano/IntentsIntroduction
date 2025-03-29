669db192ecd93350edf4ec931d1368a274d8f45e
Alejandro Lozano Medina <swlozano@gmail.com>
2025-03-28 20:34:00 -0500

    pass a value from one activity to another via intents

--- app/src/main/java/com/swlozano/intents/MainActivity.kt
+++ app/src/main/java/com/swlozano/intents/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.swlozano.intents
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+const val FULL_NAME_KEY = "FULL_NAME_KEY"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.submit_button).setOnClickListener {
+            val fullName = findViewById<EditText>(R.id.full_name).text.toString()
+            if (fullName.isNotEmpty()) {
+                val welcomeIntent = Intent(this, WelcomeActivity::class.java)
+                welcomeIntent.putExtra(FULL_NAME_KEY, fullName)
+                startActivity(welcomeIntent)
+            } else {
+                Toast.makeText(this, getString(R.string.full_name_label), Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/swlozano/intents/WelcomeActivity.kt
+++ app/src/main/java/com/swlozano/intents/WelcomeActivity.kt
@@ -1,6 +1,7 @@
 package com.swlozano.intents
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,11 @@ class WelcomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        if (intent != null) {
+            val fullName = intent.getStringExtra(FULL_NAME_KEY)
+            findViewById<TextView>(R.id.welcome_text).text =
+                getString(R.string.welcome_text, fullName)
         }
     }
 }
